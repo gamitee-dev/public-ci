f841a43807ef8f1e27fafdc12d29171512c7b53a
Mo Kamioner <mo@gamitee.com>
2022-03-20 14:15:11 +0200

    Check issues are linked and open.

--- code/commit_linter.py
+++ code/commit_linter.py
@@ -1,8 +1,12 @@
 from dataclasses import dataclass
-from typing import Any, Dict, List
+from typing import Any, Dict, Generator, List, Literal, Optional, Set
+import argparse
 import json
 import re
 import sys
+import urllib.error
+import urllib.parse
+import urllib.request
 
 MAX_COMMIT_CHARS = 72
 SUBJECT_REGEX = re.compile(r"^\S[\S ]*\S: \S[\S ]*\S\.$")
@@ -17,6 +21,110 @@ URL_REGEX = re.compile(
     r"(?:/?|[/?]\S+)$",
     re.IGNORECASE,
 )
+LINKED_ISSUES_REGEX = re.compile(
+    r"(?:(?:close|resolve)[sd]?|fix|fixe[sd])\s+"
+    r"(?:(?:(?P<owner1>[a-z0-9-_]+)\/(?P<repo1>[a-z0-9-_]+))?"
+    r"#|https:\/\/github\.com\/"
+    r"(?P<owner2>[a-z0-9-_]+)\/(?P<repo2>[a-z0-9-_]+)\/issues\/)(?P<number>\d+)",
+    re.IGNORECASE,
+)
+PULL_REQUEST_URL_PATTERN = re.compile(
+    r"https\:\/\/github\.com\/(?P<owner>[\w-]+)\/(?P<repo>[\w-]+)\/pull\/\d+$"
+)
+
+# https://dev.to/bowmanjd/http-calls-in-python-without-requests-or-other-external-dependencies-5aj1
+@dataclass
+class HttpResponse:
+    body: str
+    headers: Dict[str, str]
+    status: int
+    error_count: int = 0
+
+    def json(self) -> Dict[str, Any]:
+        return json.loads(self.body)
+
+
+def request(
+    url: str,
+    method: Literal["GET", "POST", "PUT", "DELETE", "PATCH"] = "GET",
+    data: Optional[Dict[str, Any]] = None,
+    params: Optional[Dict[str, str]] = None,
+    headers: Optional[Dict[str, str]] = None,
+) -> HttpResponse:
+    request_data = None
+
+    final_headers = {"accept": "application/json", **(headers or {})}
+
+    final_url = url
+    if params:
+        final_url += "?" + urllib.parse.urlencode(params, doseq=True, safe="/")
+
+    if data:
+        request_data = json.dumps(data).encode()
+        final_headers["content-type"] = "application/json; charset=UTF-8"
+
+    httprequest = urllib.request.Request(
+        final_url, data=request_data, headers=final_headers, method=method
+    )
+
+    try:
+        with urllib.request.urlopen(httprequest) as httpresponse:
+            return HttpResponse(
+                headers=dict(httpresponse.headers),
+                status=httpresponse.status,
+                body=httpresponse.read().decode(httpresponse.headers.get_content_charset("utf-8")),
+            )
+    except urllib.error.HTTPError as error:
+        return HttpResponse(
+            body=str(error.reason),
+            headers=dict(error.headers),
+            status=error.code,
+        )
+
+
+@dataclass
+class GithubIssue:
+    number: int
+    owner: str
+    repo: str
+    state: Literal["open", "closed"]
+    title: str
+    labels: Set[str]
+
+    def display(self) -> str:
+        return f"{self.owner}/{self.repo}#{self.number}: {self.title}"
+
+
+class GithubClient:
+    def __init__(self, token: str) -> None:
+        self._token = token
+
+    def get_issue(self, owner: str, repo: str, number: int) -> GithubIssue:
+        response = self._request(f"/repos/{owner}/{repo}/issues/{number}")
+
+        data = response.json()
+
+        return GithubIssue(
+            number=number,
+            owner=owner,
+            repo=repo,
+            state=data["state"],
+            title=data["title"],
+            labels={label["name"] for label in data["labels"]},
+        )
+
+    def _request(
+        self, path: str, method: Literal["GET", "POST", "PUT", "DELETE", "PATCH"] = "GET"
+    ) -> HttpResponse:
+        url = f"https://api.github.com{path}"
+        headers = {"authorization": f"token {self._token}"}
+
+        response = request(url=url, method=method, headers=headers)
+        if not 200 <= response.status < 300:
+            print(response, flush=True)
+            raise RuntimeError("Invalid response from github")
+
+        return response
 
 
 @dataclass
@@ -48,6 +156,16 @@ class Commit:
         )
 
 
+@dataclass(repr=False)
+class IssueDetails:
+    number: int
+    owner: str
+    repo: str
+
+    def display(self) -> str:
+        return f"{self.owner}/{self.repo}#{self.number}"
+
+
 @dataclass
 class PullRequest:
     commits: List[Commit]
@@ -55,6 +173,7 @@ class PullRequest:
     head_ref_name: str
     title: str
     url: str
+    body: str
 
     @staticmethod
     def parse(raw_data: Dict[str, Any]) -> "PullRequest":
@@ -65,6 +184,7 @@ class PullRequest:
             head_ref_name=raw_data["headRefName"],
             title=raw_data["title"],
             url=raw_data["url"],
+            body=raw_data["body"],
         )
 
     def validate_release_pr(self) -> bool:
@@ -83,6 +203,26 @@ class PullRequest:
             all(author.login == "dependabot[bot]" for author in commit.authors)
             for commit in self.commits
         )
+
+    @property
+    def owner(self) -> str:
+        return PULL_REQUEST_URL_PATTERN.match(self.url)["owner"]  # type: ignore
+
+    @property
+    def repo(self) -> str:
+        return PULL_REQUEST_URL_PATTERN.match(self.url)["repo"]  # type: ignore
+
+    def get_linked_issues(self) -> Generator[IssueDetails, None, None]:
+        default_owner = self.owner
+        default_repo = self.repo
+
+        for body_line in self.body.split("\n"):
+            for match in LINKED_ISSUES_REGEX.finditer(body_line.strip()):
+                yield IssueDetails(
+                    number=int(match["number"]),
+                    owner=(match["owner1"] or match["owner2"] or default_owner),
+                    repo=(match["repo1"] or match["repo2"] or default_repo),
+                )
 
 
 class CommitLinter:
@@ -178,7 +318,24 @@ class CommitLinter:
         return bool(re.match(URL_REGEX, last_element))
 
 
-def main(raw_data: Dict[str, Any]) -> None:
+def validate_linked_issues(pull_request: PullRequest, github_client: GithubClient) -> None:
+    print("Checking linked issues", flush=True)
+    linked_issues = list(pull_request.get_linked_issues())
+    if not linked_issues:
+        raise RuntimeError("No linked issues were found in this pull request")
+
+    for linked_issue in linked_issues:
+        issue = github_client.get_issue(
+            owner=linked_issue.owner, repo=linked_issue.repo, number=linked_issue.number
+        )
+
+        if issue.state != "open":
+            raise RuntimeError(f"Issue {issue.display()} is not open")
+
+        print(f"VALID: {issue.display()}", flush=True)
+
+
+def main(raw_data: Dict[str, Any], github_token: str) -> None:
     pull_request = PullRequest.parse(raw_data)
     print(f"Validating PR {pull_request.url}", flush=True)
     if pull_request.is_dependabot_pr():
@@ -201,12 +358,29 @@ def main(raw_data: Dict[str, Any]) -> None:
     CommitLinter.lint_title(pull_request.title)
     print("Pull request is valid", flush=True)
 
+    github_client = GithubClient(github_token)
+    validate_linked_issues(pull_request, github_client)
+
 
 if __name__ == "__main__":
-    raw_data_json = json.loads(sys.stdin.buffer.read())
+    parser = argparse.ArgumentParser(
+        description="Validate a pull request and its commit are per spec"
+    )
+    parser.add_argument("--token", help="The token to access github", required=True)
+    parser.add_argument(
+        "input_file",
+        nargs="?",
+        help="The file from github CLI with details of the PR",
+        type=argparse.FileType("r"),
+        default=sys.stdin,
+    )
+
+    args = parser.parse_args()
+    raw_data_json = json.loads(args.input_file.buffer.read())
+
     try:
-        main(raw_data_json)
-    except RuntimeError as error:
-        print(f"!! {error.args[0]}", flush=True)
+        main(raw_data_json, args.token)
+    except RuntimeError as run_error:
+        print(f"!! {run_error.args[0]}", flush=True)
         print("Linting failed", flush=True)
         sys.exit(1)
